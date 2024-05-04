41c81d82fe939cf30f3a301f9a58f02f033957da
sethdaking <105960840+sethdaking@users.noreply.github.com>
2024-05-04 10:09:00 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,5 +2,5 @@
  
 export default function myImageLoader({ src, width, quality }) {
     if(src.startsWith('https://i.ibb.co')) return src
-  return `https://techbuds-portfolio.com/${src}?w=${width}&q=${quality || 75}`
+  return `https://techbuds-portfolio.vercel.app//${src}?w=${width}&q=${quality || 75}`
 }
